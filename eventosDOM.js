2c42b3a8cab6d816bb691934d3890a4b07ee3501
raulmunozs <ralexanderms@gmail.com>
2022-09-28 23:43:44 -0400

    intento de carrito

--- eventosDOM.js
+++ eventosDOM.js
@@ -22,73 +22,78 @@ const producto1 = new producto(1,"Bullet Journal", 100,"AlephLa técnica ha ido 
 
 const producto2 = new producto(2,"Lectura Agil", 200,"Es un conjunto de métodos y también de técnicas que tienen como principal propósito, el aumentar la velocidad al momento de leer, pero esto no significa que se va a reducir ni el nivel de comprensión, ni tampoco el hecho de retener la información.", "./bancoDeImagenes/pexels-christina-morillo-1181595.jpg")
 
-const producto3 = new producto(3,"Economia para emprebdedores", 300, "Los emprendedores son personas que combinan sus recursos y trabajo para crear activos de una empresa, sin embargo, gestionar la ecomia no es sencillo. Este curso se comprende por varios bloques: Plan de ventas previsional. Presupuesto de gastos previsional. Plan de inversiones y financiación previsional.", "./bancoDeImagenes/pexels-pixabay-209224.jpg")
+const producto3 = new producto(3,"Economia para emprendedores", 300, "Los emprendedores son personas que combinan sus recursos y trabajo para crear activos de una empresa, sin embargo, gestionar la ecomia no es sencillo. Este curso se comprende por varios bloques: Plan de ventas previsional. Presupuesto de gastos previsional. Plan de inversiones y financiación previsional.", "./bancoDeImagenes/pexels-pixabay-209224.jpg")
 
 
 //Dos formas inicializar el array:
 //const biblioteca = [producto1, producto2, producto3]
 // console.log(biblioteca)
 //Segunda forma
-const estanteria= []
+let estanteria= []
 estanteria.push(producto1, producto2, producto3)
-let sectionCursos = document.getElementById("carrito")
-
-    estanteria.forEach((producto)=>{
-        let nuevoCurso = document.createElement(`div`);
-        nuevoCurso.innerHTML = `
-                    <h2 data-aos="fade-up" data-aos-duration="3000">Cursos</h2>
-                    <div class="row justify-content-around mt-5">
-                    <div class="card col-4 mb-3" style="width: 18rem;">
+let sectionCursos = document.getElementById("curses")
+    console.log(sectionCursos)
+estanteria.forEach( (producto) => {
+    let nuevoCurso = document.createElement("div")
+    nuevoCurso.innerHTML = `<div class="container-fluid row justify-content-around mt-5">
+                    <div id= "${producto.id} class="card col-4 mb-3" style="width: 18rem;">
                         <img src="${producto.imagen}" class="card-img-top pt-2" alt="Bullet Journal, control de ansiedad">
                         <div class="card-body ">
                         <h5 class="card-title">${producto.titulo}</h5>
                         <p class="card-text">${producto.precio}</p>
-                        <a href="#" class=" comprar btn btn-primary">Ir al curso</a>
+                        <a href="#" id= "compra" class="btn btn-primary">Comprar</a>
                         </div>
                     </div>
-                    </div>
-                    <div class="col-4">
-            <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                    value="1">
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
-            </div>
+                    </div>`
+    sectionCursos.appendChild(nuevoCurso)
+})
+   
+ let btnCompra = document.getElementsById("comprar")
+  for(let compra of btnCompra){
+  compra.addEventListener("click", ()=>{
+       alert("El cuso fue añadido al carrito")
+    })
+  }
+
+//Clase STORAGE
+
+
+//DOM CARRITO
+let botonCarrito = document.getElementById("botonCarrito")
+let modalBody = document.getElementById("modal-body")
+let botonFinalizarCompra = document.getElementById("botonFinalizarCompra")
+let parrafoCompra = document.getElementById('precioTotal')
+
+botonCarrito.addEventListener("click", ()=>{
+    cargarProductosCarrito(productosEnCarrito)
+})
+function cargarProductosCarrito(array){
+
+    modalBody.innerHTML = ""
+    array.forEach((productoCarrito)=>{
+
+        modalBody.innerHTML += `
+        <div class="card border-primary mb-3" id ="productoCarrito${productoCarrito.id}" style="max-width: 540px;">
+            <img class="card-img-top" height="300px" src="assets/${productoCarrito.imagen}" alt="${productoCarrito.titulo}">
+            <div class="card-body">
+                    <h4 class="card-title">${productoCarrito.titulo}</h4>
+                
+                    <p class="card-text">$${productoCarrito.precio}</p> 
+                    <button class= "btn btn-danger" id="botonEliminar"><i class="fas fa-trash-alt"></i></button>
+            </div>    
+        
+        
         </div>
-    </div>
-                `
-        sectionCursos.append(nuevoCurso)
+`
+
     })
-    let btnCompra = document.getElementsByClassName("comprar")
-    for(let compra of btnCompra){
-    compra.addEventListener("click", ()=>{
-        alert("El cuso fue añadido al carrito")
-    })
-
+    //calcular el total
+    compraTotal(array)
 }
 
-const suscriptor = []
-// //function agregar Libro
-function nuevosuscriptor(array){
-    let nombreIngresado = prompt("Ingrese el nombre")
-    let emailIngresado = prompt("Ingrese el correo")
-    let suscriptorCreado = new Libro (suscriptor.length+1, nombreIngresado, emailIngresado)
-    array.push(suscriptorCreado)
-    
-}
+function compraTotal(array){
+    let acumulador = 0
 
-//function nuevoLibro actualiza a inputs!
-function guardarSuscriptor(array){
-    let emailInput = document.getElementById("exampleInputEmail1")
-    let nombreInput = document.getElementById("exampleInputText1")
-    let suscriptorCreado = new Libro (array.length+1, emailInput.value, tituloInput.value, parseInt(precioInput.value), "libroNuevo.jpg")
-    console.log(suscriptorCreado)
-    array.push(suscriptorCreado)
-    console.log(array)
-    //Provisorio resetear form
-    nombreInput.value = ""
-    emailInput.value = ""
-    
-    mostrarCatalogo(array)
-}
-
+    acumulador = array.reduce((acumulador, productoCarrito)=>{
+        return acumulador + productoCarrito.precio
+    },0)*/
